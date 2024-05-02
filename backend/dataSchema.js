258a0eadbbccc46d56f3209112fe6736b721e654
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-05-02 21:06:57 +0000

    Schema update

--- backend/dataSchema.js
+++ backend/dataSchema.js
@@ -1,19 +1,76 @@
 const mongoose = require('mongoose')
 
-const ReactFormDataSchema = new mongoose.Schema({
+const ReactFormDataSchemaWater = new mongoose.Schema({
     _id: { type: Number },
+    waterID: { type: Number},
     title: { type: String },
-    price: { type: Number },
+    url: { type: String },
     description: { type: String },
-    category: { type: String },
-    image: { type: String },
-    rating: {
-        rate: { type: Number },
-        count: { type: Number }
-    }
+    macro_img: { type: String },
 },
-    { collection: "fakestore_catalog" }
-)
+{ collection: "water" });
 
-const Product = mongoose.model('Product', ReactFormDataSchema)
-module.exports = Product
+const WaterProduct = mongoose.model('WaterProduct', ReactFormDataSchemaWater)
+
+//------------------------------------------------------------------------------------
+
+const ReactFormDataSchemaJuice = new mongoose.Schema({
+    _id: { type: Number },
+    juiceID: { type: Number},
+    title: { type: String },
+    url: { type: String },
+    description: { type: String },
+    macro_img: { type: String },
+},
+{ collection: "juice" });
+
+const JuiceProduct = mongoose.model('JuiceProduct', ReactFormDataSchemaJuice)
+
+//------------------------------------------------------------------------------------
+
+const ReactFormDataSchemaSoda = new mongoose.Schema({
+    _id: { type: Number },
+    sodaID: { type: Number},
+    title: { type: String },
+    url: { type: String },
+    description: { type: String },
+    macro_img: { type: String },
+},
+{ collection: "soda" });
+
+const SodaProduct = mongoose.model('SodaProduct', ReactFormDataSchemaSoda)
+
+//------------------------------------------------------------------------------------
+
+const ReactFormDataSchemaBeer = new mongoose.Schema({
+    _id: { type: Number },
+    beerID: { type: Number},
+    title: { type: String },
+    url: { type: String },
+    description: { type: String },
+    macro_img: { type: String },
+},
+{ collection: "beer" });
+
+const BeerProduct = mongoose.model('BeerProduct', ReactFormDataSchemaBeer)
+
+//------------------------------------------------------------------------------------
+
+const ReactFormDataSchemaReview = new mongoose.Schema({
+    _id: { type: Number },
+    reviewID: { type: Number},
+    productName: { type: String },
+    comment: { type: String },
+    rating: {type: String }
+},
+{ collection: "review" });
+
+const ReviewProduct = mongoose.model('ReviewProduct', ReactFormDataSchemaReview)
+
+module.exports = {
+    WaterProduct,
+    JuiceProduct,
+    SodaProduct,
+    BeerProduct,
+    ReviewProduct
+}

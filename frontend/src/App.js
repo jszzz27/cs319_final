625123b2c542634a68f06314ef7724e71da7ffd4
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-05-04 19:47:06 +0000

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -339,26 +339,26 @@ function App() {
           </div>
 
           <div className="row mt-6">
-              <div className="col-md-7">
-                  <img id="profile" src="./myotherimages/junsange.jpg" alt="junsang" width="160px" height="200px" />
-                  <h2 className="fw-normal">Jun-Sang Kim</h2>
-                  <p id="email">junsange@iastate.edu</p>
-                  <p>My name is Jun-Sang Kim, and I am a junior at Iowa State University majoring in Software Engineering. This is my first time learning about HTML, CSS, JavaScript, and web development, which means all the techniques and skills I am developing and utilizing here are basically all from the content of this course. However, as a student majoring in Software Engineering field, I have learned various types of languages and skills, such as C, C++, Java, Android Studio, MySQL, and Git, that can be transferred to this course and are actually helpful for me to understand the content easily.</p>
-              </div>
-              <div className="col-md-7">
-                  <h2>About Us</h2>
-                  <p>Iowa State University - May 5th, Spring 2024</p>
-                  <p>SE / COM S 319 - Construction of User Interfaces</p>
-                  <p>We are here to provide you a chance to select the best beverage in your lifetime.</p>
-                  <p>Welcome to the Paradise.</p>
-              </div>
-              <div className="col-md-7">
-                  <img id="profile" src="./myotherimages/antonio.jpg" alt="antonio" width="160px" height="200px"/>
-                  <h2 className="fw-normal">Antonio Perez</h2>
-                  <p id="email">avperez@iastate.edu</p>
-                  <p>My name is Antonio Perez; I am a senior here at Iowa State University majoring in Aerospace Engineering and minoring in Computer Science. While my background is mainly in engineering and design, I have experience with VS code, Git, and various coding languages. This is my first course where I have been introduced to HTML, CSS and Javascript. However, my skillset has aligned me for quick learning and ample problem-solving skills. These will prove essential for the midterm and the work that will come beyond this project's scope.</p>
-              </div>
-          </div>
+            <div className="col-md-4">
+                <img id="profile" src="./myotherimages/junsange.jpg" alt="junsang" width="160px" height="200px" />
+                <h2 className="fw-normal">Jun-Sang Kim</h2>
+                <p id="email">junsange@iastate.edu</p>
+                <p>My name is Jun-Sang Kim, and I am a junior at Iowa State University majoring in Software Engineering. This is my first time learning about HTML, CSS, JavaScript, and web development, which means all the techniques and skills I am developing and utilizing here are basically all from the content of this course. However, as a student majoring in Software Engineering field, I have learned various types of languages and skills, such as C, C++, Java, Android Studio, MySQL, and Git, that can be transferred to this course and are actually helpful for me to understand the content easily.</p>
+            </div>
+            <div className="col-md-4">
+                <h2>About Us</h2>
+                <p>Iowa State University - May 5th, Spring 2024</p>
+                <p>SE / COM S 319 - Construction of User Interfaces</p>
+                <p>We are here to provide you a chance to select the best beverage in your lifetime.</p>
+                <p>Welcome to the Paradise.</p>
+            </div>
+            <div className="col-md-4">
+                <img id="profile" src="./myotherimages/antonio.jpg" alt="antonio" width="160px" height="200px"/>
+                <h2 className="fw-normal">Antonio Perez</h2>
+                <p id="email">avperez@iastate.edu</p>
+                <p>My name is Antonio Perez; I am a senior here at Iowa State University majoring in Aerospace Engineering and minoring in Computer Science. While my background is mainly in engineering and design, I have experience with VS code, Git, and various coding languages. This is my first course where I have been introduced to HTML, CSS and Javascript. However, my skillset has aligned me for quick learning and ample problem-solving skills. These will prove essential for the midterm and the work that will come beyond this project's scope.</p>
+            </div>
+        </div>
       </div>
       )}
 

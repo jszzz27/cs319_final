01199d993cc90347c6387fa90a299e1a257785e5
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-05-04 00:09:23 +0000

    car

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -299,10 +299,41 @@ function App() {
       </div>
 
       <div className='m-4'>
-       {menu === 1 && <div>
+      {menu === 1 && (<div>
           <h1 className='text-center text-danger'>Main</h1>
           <hr></hr>
-        </div>}
+          <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img className="d-block w-100" src="carousel-image-1.jpg" alt="First slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100" src="carousel-image-2.jpg" alt="Second slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100" src="carousel-image-3.jpg" alt="Third slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100" src="carousel-image-4.jpg" alt="Fourth slide" />
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      )}
 
         {menu === 2 && <div>
           <h1 className='text-center text-danger'>Beer</h1>

99ec7e6d23002116c63bb2ec235c15eaf99c3271
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-05-05 18:54:01 +0000

    patch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1004,7 +1004,7 @@ function App() {
             <div>
               <div className='selected-product'>
                 <div className='selected-product-img'>
-                  <img src={selectedProduct.url} style={{ marginLeft: `50px`}} alt={selectedProduct.title} />
+                  <img src={selectedProduct.url} style={{ marginLeft: `200px`}} alt={selectedProduct.title} />
                 </div>
                 <div className='selected-product-details'>
                   <h2>{selectedProduct.title}</h2>
@@ -1172,7 +1172,7 @@ function App() {
             <div>
               <div className='selected-product'>
                 <div className='selected-product-img'>
-                  <img src={selectedProduct.url} alt={selectedProduct.title} />
+                  <img src={selectedProduct.url} style={{ marginLeft: `200px`}} alt={selectedProduct.title} />
                 </div>
                 <div className='selected-product-details'>
                   <h2>{selectedProduct.title}</h2>
@@ -1361,7 +1361,7 @@ function App() {
             <div>
               <div className='selected-product'>
                 <div className='selected-product-img'>
-                  <img src={selectedProduct.url} alt={selectedProduct.title} />
+                  <img src={selectedProduct.url} style={{ marginLeft: `200px`}} alt={selectedProduct.title} />
                 </div>
                 <div className='selected-product-details'>
                   <h2>{selectedProduct.title}</h2>
@@ -1541,7 +1541,7 @@ function App() {
             <div>
               <div className='selected-product'>
                 <div className='selected-product-img'>
-                  <img src={selectedProduct.url} alt={selectedProduct.title} />
+                  <img src={selectedProduct.url} style={{ marginLeft: `200px`}} alt={selectedProduct.title} />
                 </div>
                 <div className='selected-product-details'>
                   <h2>{selectedProduct.title}</h2>
